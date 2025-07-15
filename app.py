58a355ab09de42477d243198c3082f775a7973cf
Alphalejo <keatola16.ktl@gmail.com>
2025-07-15 07:35:31 -0500

    Added comparasion chart, created new UI blocks

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 import utils.api as api
 import utils.charts as charts
+import utils.ui_blocks as ui_blocks
 
 #App title
 st.title("PokeDex")
@@ -37,18 +38,14 @@ if function == "Information":
         print(asset, asset_name) # Debugging line to check the asset and name
         
         if asset == "pokemon":
-            api.pokemon_data(asset, asset_name)
+            ui_blocks.pokemon_data(asset, asset_name)
                 
         else:
             st.image("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages-wixmp-ed30a86b8c4ca887773594c2.wixmp.com%2Ff%2F5d593771-5eab-464a-91a2-1e4af6b88b2f%2Fddrl3x8-f0050a0d-450f-4394-b71b-8bccef18504d.png%2Fv1%2Ffill%2Fw_640%2Ch_256%2Cstrp%2Fpokemon_berries_1_10_128px_artworks_by_anarlaurendil_ddrl3x8-fullview.png%3Ftoken%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjU2IiwicGF0aCI6IlwvZlwvNWQ1OTM3NzEtNWVhYi00NjRhLTkxYTItMWU0YWY2Yjg4YjJmXC9kZHJsM3g4LWYwMDUwYTBkLTQ1MGYtNDM5NC1iNzFiLThiY2NlZjE4NTA0ZC5wbmciLCJ3aWR0aCI6Ijw9NjQwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.8luKHvaPYn40DvpnqJ186noQ7b5Y9JvW7zRQ1QO3-P0&f=1&nofb=1&ipt=8ea3b6307cd1d50b64b6382a52f0aef30b44d97840a1b164f650af7a33102755")
 
 elif function == "Compare":
-    st.write("Compare Pokemon features")
-    pokemones_to_compare = st.multiselect(
-        "Select Pokemons to compare:",
-        options= api.all_pokemon_names(),
-        max_selections=5
-    )        
+    
+    ui_blocks.compare_dashboard()
 
 else:
     st.write("Please select a valid function.")
--- utils/api.py
+++ utils/api.py
@@ -22,40 +22,6 @@ def get_data(asset="", name="", params=None):
 
 
 #_____________________________________________________________________________________________________
-# Function to fetch data for all Pokemon
-# This function fetches data for all Pokemon and displays their names in a multiselect widget
-def pokemon_data(asset="", asset_name=""):
-    """
-    Fetches data for all Pokemon.
-    """
-    data = get_data(asset, asset_name)
-
-    type_names = [type["type"]["name"] for type in data.get("types", [])]
-    col1, col2 = st.columns([1,3])
-
-    with col1:
-        st.image(data["sprites"]["front_default"], use_container_width=True)
-        st.markdown(f"<h3 style='text-align: center;'>#{data['id']}</h3>", unsafe_allow_html=True)
-    
-    with col2:
-        st.header(f"{asset_name.capitalize()}")
-        #____________________________________________________________________________________________________
-        badges = "".join([styles.pokemon_type(type_name) for type_name in type_names])
-        st.markdown(
-            f"""
-            <div style='display: flex; gap: 8px; flex-wrap: wrap;'>
-                {badges}
-            """,
-            unsafe_allow_html=True
-        )
-      #______________________________________________________________________________________________________
-        st.text(f"height: {data['height']}")
-        st.text(f"weight: {data['weight']}")
-        st.text(f"base experience: {data['base_experience']}")
-    
-    st.plotly_chart(charts.pokemon_stats_chart(asset=asset, name=asset_name), use_container_width=True)
-
-#_____________________________________________________________________________________________________
 # Function to fetch data for all Pokemon names
 # This function fetches data for all Pokemon and displays their names in a multiselect widget
 
@@ -67,3 +33,10 @@ def all_pokemon_names():
     return pokemon_names
 
 #_____________________________________________________________________________________________________
+
+def all_pokemon_types():
+    """
+    Fetches all Pokemon types.
+    """
+    response = requests.get(url + "type").json()
+    return [type_info['name'] for type_info in response['results']]
