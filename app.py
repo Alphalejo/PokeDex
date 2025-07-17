a556c748fbb6576d2dbd2b11434cd72d3a88d603
Alphalejo <keatola16.ktl@gmail.com>
2025-07-17 06:26:09 -0500

    created damage functions for VS

--- app.py
+++ app.py
@@ -40,6 +40,8 @@ if function == "Information":
         
         if asset == "pokemon":
             ui_blocks.pokemon_data(asset, asset_name)
+            st.plotly_chart(charts.pokemon_stats_chart(asset=asset, name=asset_name), use_container_width=True)
+
                 
         else:
             st.image("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages-wixmp-ed30a86b8c4ca887773594c2.wixmp.com%2Ff%2F5d593771-5eab-464a-91a2-1e4af6b88b2f%2Fddrl3x8-f0050a0d-450f-4394-b71b-8bccef18504d.png%2Fv1%2Ffill%2Fw_640%2Ch_256%2Cstrp%2Fpokemon_berries_1_10_128px_artworks_by_anarlaurendil_ddrl3x8-fullview.png%3Ftoken%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjU2IiwicGF0aCI6IlwvZlwvNWQ1OTM3NzEtNWVhYi00NjRhLTkxYTItMWU0YWY2Yjg4YjJmXC9kZHJsM3g4LWYwMDUwYTBkLTQ1MGYtNDM5NC1iNzFiLThiY2NlZjE4NTA0ZC5wbmciLCJ3aWR0aCI6Ijw9NjQwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.8luKHvaPYn40DvpnqJ186noQ7b5Y9JvW7zRQ1QO3-P0&f=1&nofb=1&ipt=8ea3b6307cd1d50b64b6382a52f0aef30b44d97840a1b164f650af7a33102755")
--- utils/api.py
+++ utils/api.py
@@ -1,8 +1,4 @@
-import streamlit as st
-
 import requests
-import utils.styles as styles
-import utils.charts as charts
 
 # Connect to the API
 url = "https://pokeapi.co/api/v2/"
@@ -20,6 +16,17 @@ def get_data(asset="", name="", params=None):
         print(f"An error occurred: {e}")
         return None
 
+#_____________________________________________________________________________________________________
+# Function to fetch the types of a given Pokemon
+
+def get_pokemon_types(pokemon_name):
+    """
+    Fetches the types of a given Pokemon.
+    """
+    data = get_data(asset="pokemon", name=pokemon_name)
+    if data and "types" in data:
+        return [type_info['type']['name'] for type_info in data['types']]
+    return []
 
 #_____________________________________________________________________________________________________
 # Function to fetch data for all Pokemon names
@@ -33,6 +40,7 @@ def all_pokemon_names():
     return pokemon_names
 
 #_____________________________________________________________________________________________________
+# Function to fetch all Pokemon types
 
 def all_pokemon_types():
     """
@@ -42,6 +50,7 @@ def all_pokemon_types():
     return [type_info['name'] for type_info in response['results']]
 
 #_____________________________________________________________________________________________________
+# Function to fetch the sprite URL for a given Pokemon name
 
 def get_pokemon_sprite(pokemon_name):
     """
@@ -51,3 +60,85 @@ def get_pokemon_sprite(pokemon_name):
     if data and "sprites" in data and "front_default" in data["sprites"]:
         return data["sprites"]["front_default"]
     return None
+
+#_____________________________________________________________________________________________________
+# Function to fetch damage relations for a given Pokemon
+
+def get_damage_multipliers(pokemon):
+    """
+    Fetches damage relations for a given Pokemon.
+    """
+    types = get_pokemon_types(pokemon)
+    data = get_data(asset="type", name=types[0]) if types else None
+
+    if types:
+        #Create lists to store damage relations
+        double_damage_from = [data['damage_relations']['double_damage_from'][i]['name'] for i in range(len(data['damage_relations']['double_damage_from']))]
+        double_damage_to = [data['damage_relations']['double_damage_to'][i]['name'] for i in range(len(data['damage_relations']['double_damage_to']))]
+        half_damage_from = [data['damage_relations']['half_damage_from'][i]['name'] for i in range(len(data['damage_relations']['half_damage_from']))]
+        half_damage_to = [data['damage_relations']['half_damage_to'][i]['name'] for i in range(len(data['damage_relations']['half_damage_to']))]
+        no_damage_from = [data['damage_relations']['no_damage_from'][i]['name'] for i in range(len(data['damage_relations']['no_damage_from']))]
+        no_damage_to = [data['damage_relations']['no_damage_to'][i]['name'] for i in range(len(data['damage_relations']['no_damage_to']))]
+
+        if len(types) > 1:
+            # If the Pokemon has more than one type, fetch damage relations for the second type
+            data = get_data(asset="type", name=types[1])
+            double_damage_from += [data['damage_relations']['double_damage_from'][i]['name'] for i in range(len(data['damage_relations']['double_damage_from']))]
+            double_damage_to += [data['damage_relations']['double_damage_to'][i]['name'] for i in range(len(data['damage_relations']['double_damage_to']))]
+            half_damage_from += [data['damage_relations']['half_damage_from'][i]['name'] for i in range(len(data['damage_relations']['half_damage_from']))]
+            half_damage_to += [data['damage_relations']['half_damage_to'][i]['name'] for i in range(len(data['damage_relations']['half_damage_to']))]
+            no_damage_from += [data['damage_relations']['no_damage_from'][i]['name'] for i in range(len(data['damage_relations']['no_damage_from']))]
+            no_damage_to += [data['damage_relations']['no_damage_to'][i]['name'] for i in range(len(data['damage_relations']['no_damage_to']))]
+
+        else: pass
+    else: return None
+
+    #-------------------------------------------------------------
+    # Create dictionaries to store attack and defence relations (damage multipliers)
+    atack_relation = dict.fromkeys(all_pokemon_types(), 1)   
+
+    for type_name in list(set(double_damage_to)):
+        atack_relation[type_name] *= 2
+    
+    for type_name in list(set(half_damage_to)):
+        atack_relation[type_name] *= 0.5
+    
+    for type_name in list(set(no_damage_to)):
+        atack_relation[type_name] *= 0
+
+    #------------------------------------------------------------
+    defence_relation = dict.fromkeys(all_pokemon_types(), 1)
+
+    for type_name in list(set(double_damage_from)):
+        defence_relation[type_name] *= 2
+
+    for type_name in list(set(half_damage_from)):
+        defence_relation[type_name] *= 0.5
+
+    for type_name in list(set(no_damage_from)):
+        defence_relation[type_name] *= 0
+
+    return {
+        "attack": atack_relation,
+        "defence": defence_relation
+    }
+
+#_____________________________________________________________________________________________________
+# Function to calculate damage multipliers for a given Pokemon
+
+def get_damage_relations(pokemon):
+    
+    damage_multipliers = get_damage_multipliers(pokemon)
+
+    double_damage_from = damage_multipliers['defence']['double_damage_from']
+    double_damage_to
+    return {
+        "double_damage_from": double_damage_from,
+        "double_damage_to": double_damage_to,
+        "half_damage_from": half_damage_from,
+        "half_damage_to": half_damage_to,
+        "no_damage_from": no_damage_from,
+        "no_damage_to": no_damage_to}
+
+#_____________________________________________________________________________________________________
+ 
